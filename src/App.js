2b0f9e775c5b7d9f18113d64b6756e2fc7d9884d
waqar hussain <wh751212@gmail.com>
2023-11-27 12:34:02 +0500

    27/11

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
@@ -5,16 +6,39 @@ import ImageLinkForm from './components/imgaelinkform/ImageLinkForm';
 import ParticlesBg from 'particles-bg'
 
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg className='paticles' type="square" bg={true} />
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg className='paticles' type="circle" bg={true} />
+        <Navigation />
+        <Logo />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
+
 }
+
+
+// function App() {
+// }
 
 export default App;
--- src/components/imgaelinkform/ImageLinkForm.js
+++ src/components/imgaelinkform/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './imagelink.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return (
         
         <div>
@@ -10,8 +10,8 @@ const ImageLinkForm = () => {
             </p>
             <div className='center'>
                 <div className='form center pa4 ma3 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type="text" />
-                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-purple'>Detect</button>
+                    <input className='f4 pa2 w-70 center' type="text" onChange={onInputChange}/>
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-purple' onClick={onButtonSubmit}>Detect</button>
                 </div>
             </div>
         </div >
