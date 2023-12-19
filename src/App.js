af7d8d16af76b4ae79921e2ab6bbee1dcfba716c
waqarhussainujjan <wh751212@gmail.com>
2023-12-19 18:32:09 +0500

    update

--- src/App.js
+++ src/App.js
@@ -26,6 +26,19 @@ class App extends Component {
     return (
       <div className="App">
         <ParticlesBg className='paticles' type="circle" bg={true} />
+        {/* 
+        <ParticlesBg className='paticles' type="color" bg={true} />
+        <ParticlesBg className='paticles' type="ball" bg={true} />
+        <ParticlesBg className='paticles' type="lines" bg={true} />
+        <ParticlesBg className='paticles' type="thick" bg={true} />
+        <ParticlesBg className='paticles' type="cobweb" bg={true} />
+        <ParticlesBg className='paticles' type="polygon" bg={true} />
+        <ParticlesBg className='paticles' type="square" bg={true} />
+        <ParticlesBg className='paticles' type="tadpole" bg={true} />
+        <ParticlesBg className='paticles' type="fountain" bg={true} />
+        <ParticlesBg className='paticles' type="random" bg={true} />
+        <ParticlesBg className='paticles' type="custom" bg={true} /> 
+        */}
         <Navigation />
         <Logo />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
@@ -36,8 +49,6 @@ class App extends Component {
   }
 
 }
-
-
 // function App() {
 // }
 
